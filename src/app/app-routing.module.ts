92fe4768693d564cd28e4daac25a25090d8fa3fb
Deeksha Shettigar <deeksha.s@testyantra.in>
2020-01-28 10:04:52 +0530

    add

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CarDetailsComponent } from './car-details/car-details.component';
+import { CarInfoComponent } from './car-info/car-info.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'car-details', component: CarDetailsComponent },
+  { path: 'car-info', component: CarInfoComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CarDetailsComponent } from './car-details/car-details.component';
+import { CarInfoComponent } from './car-info/car-info.component';
+import { HeaderComponent } from './header/header.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarDetailsComponent,
+    CarInfoComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    Ng2SearchPipeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
